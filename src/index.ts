299d27a44f92a297ed6f73f6eea4d856d09dcb83
Ben Christel <ben.christel@gmail.com>
2024-06-12 21:11:07 -0700

    Find the search input by data attribute

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import {applySearch, homElements} from "./hom";
 import {BagOfPrefixesSearch} from "./search/bag-of-prefixes-search";
 
-export function init(search: HTMLInputElement, firstHeading: Node | null = null) {
+export function init() {
+  const search: HTMLInputElement | null = document.querySelector("input[data-quendingold]")
+  if (search == null) return;
+
   const hom = homElements(document.body)
   search.addEventListener("input", () => {
     const bagOfWords = new BagOfPrefixesSearch(search.value)
