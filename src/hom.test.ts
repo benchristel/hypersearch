0b89ae61cbdde76ac5dd169d2734eeb41e119fe8
Ben Christel <ben.christel@gmail.com>
2024-06-13 18:10:25 -0700

    Add more tests

--- src/hom.test.ts
+++ src/hom.test.ts
@@ -2,8 +2,8 @@ import {test, expect, equals} from "@benchristel/taste"
 import {HomElement, homElements} from "./hom"
 import {BagOfPrefixesSearch} from "./search/bag-of-prefixes-search"
 
-test("HOM", {
-  "when empty, given an empty query"() {
+test("an empty HOM", {
+  "returns no results for an empty query"() {
     const html = ""
     const query = ""
     const expected = [] as string[]
@@ -11,26 +11,109 @@ test("HOM", {
     verifySearchResults(html, query, expected)
   },
 
-  "when empty, given a nonempty query"() {
+  "returns no results for a nonempty query"() {
     const html = ""
     const query = "foo"
     const expected = [] as string[]
 
     verifySearchResults(html, query, expected)
   },
+})
 
-  "when nonempty, given an empty query"() {
-    const html = "<h2>Heading</h2><p>paragraph</p>"
+test("a HOM", {
+  "returns everything for an empty query"() {
+    const html = `
+      <h2>Heading</h2>
+      <p>paragraph</p>`
     const query = ""
     const expected = ["h2 Heading", "p paragraph"]
     
     verifySearchResults(html, query, expected)
   },
 
-  "when nonempty, given a nonmatching query"() {
-    const html = "<h2>Heading</h2><p>paragraph</p>"
+  "returns matching elements for a nonempty query"() {
+    const html = `
+      <h2>Heading</h2>
+      <p>paragraph</p>
+      <p>no match here</p>`
+    const query = "par"
+    const expected = ["h2 Heading", "p paragraph"]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "returns no results for a query that matches nothing"() {
+    const html = `
+      <h2>Heading</h2>
+      <p>paragraph</p>`
     const query = "foo"
     const expected = [] as string[]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "includes an h3"() {
+    const html = "<h3>Heading</h3>"
+    const query = ""
+    const expected = ["h3 Heading"]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "includes an h4"() {
+    const html = "<h4>Heading</h4>"
+    const query = ""
+    const expected = ["h4 Heading"]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "includes an h5"() {
+    const html = "<h5>Heading</h5>"
+    const query = ""
+    const expected = ["h5 Heading"]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "includes an h6"() {
+    const html = "<h6>Heading</h6>"
+    const query = ""
+    const expected = ["h6 Heading"]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "only shows the headings for sections with results"() {
+    const html = `
+      <h2>111</h2>
+      <p>aaa</p>
+      <h2>222</h2>
+      <p>bbb</p>`
+    const query = "b"
+    const expected = ["h2 222", "p bbb"]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "given a query that matches a section heading, shows content in that section"() {
+    const html = `
+      <h2>111</h2>
+      <p>aaa</p>
+      <p>bbb</p>`
+    const query = "1"
+    const expected = ["h2 111", "p aaa", "p bbb"]
+
+    verifySearchResults(html, query, expected)
+  },
+
+  "given a query that matches a section heading, shows subsection headings"() {
+    const html = `
+      <h2>111</h2>
+      <h3>222</h3>
+      <p>aaa</p>`
+    const query = "1"
+    const expected = ["h2 111", "h3 222", "p aaa"]
 
     verifySearchResults(html, query, expected)
   },
